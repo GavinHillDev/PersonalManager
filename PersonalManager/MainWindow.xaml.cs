a34336585467c1ec0ce5bdc268ab3db7262fdd4f
Gavin <88601252+GavinHillDev@users.noreply.github.com>
2026-04-09 00:39:50 -0400

    first commit

---
+++ PersonalManager/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using PersonalManager.PageClasses;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using static System.Net.Mime.MediaTypeNames;
+using PersonalManager.Windows;
+namespace PersonalManager
+{
+
+    public partial class MainWindow : NavigationWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+
+        }
+
+        }
+}
